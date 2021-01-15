0636b8beac58b315663112111d83ca91538b06d6
Raikuman <potatopiehotcakes@gmail.com>
2021-01-14 17:23:27 -0800

    Implemented TextChannelListener CommandManager now handles events

--- src/main/java/com/reliquary/crow/listeners/TextChannelListener.java
+++ src/main/java/com/reliquary/crow/listeners/TextChannelListener.java
@@ -1,20 +1,46 @@
 package com.reliquary.crow.listeners;
 
+import com.reliquary.crow.commands.manager.CommandManager;
+import com.reliquary.crow.resources.configs.ConfigHandler;
 import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Nonnull;
 
 public class TextChannelListener extends ListenerAdapter {
+
+	private static final Logger logger = LoggerFactory.getLogger(TextChannelListener.class);
+	private final CommandManager manager = new CommandManager();
+
+	@Override
+	public void onReady(@Nonnull ReadyEvent event) {
+		logger.info("{} TextListener is initialized", event.getJDA().getSelfUser().getAsTag());
+	}
 
 	@Override
 	public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent event) {
 
 		User user = event.getAuthor();
 
-		// Check if author is bot
+		// Check if author is bot, return
 		if (user.isBot() || event.isWebhookMessage())
 			return;
 
+		// Check for prefix, then handle event
+		ConfigHandler configHandler = new ConfigHandler();
+
+		String prefix = configHandler.loadConfigSetting(
+			"botSettings",
+			"prefix"
+		);
+		String raw = event.getMessage().getContentRaw();
+
+		if (raw.startsWith(prefix))
+			manager.handle(event);
 	}
 }
