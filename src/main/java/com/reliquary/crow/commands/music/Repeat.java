53f422c73e53ac152df23023e1911bc2f2684b0e
Raikuman <potatopiehotcakes@gmail.com>
2021-11-04 15:26:58 -0700

    Completed Repeat Command Cleaned up command code and added documentation

--- src/main/java/com/reliquary/crow/commands/music/Repeat.java
+++ src/main/java/com/reliquary/crow/commands/music/Repeat.java
@@ -4,18 +4,21 @@ import com.reliquary.crow.commands.manager.CommandContext;
 import com.reliquary.crow.commands.manager.CommandInterface;
 import com.reliquary.crow.commands.music.manager.GuildMusicManager;
 import com.reliquary.crow.commands.music.manager.PlayerManager;
+import com.reliquary.crow.resources.MessageMaker;
 import com.reliquary.crow.resources.RandomClasses.RandomColor;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.GuildVoiceState;
 import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.TextChannel;
 
-import java.time.Duration;
-
+/**
+ * This class handles setting the music manager to repeat the current track
+ *
+ * @version 1.0
+ * @since 2021-04-11
+ */
 @SuppressWarnings("ConstantConditions")
 public class Repeat implements CommandInterface {
 
@@ -26,33 +29,35 @@ public class Repeat implements CommandInterface {
 		final Member self = ctx.getSelfMember();
 		final GuildVoiceState selfVoiceState = self.getVoiceState();
 
-		// Check if a user is in a voice channel
-		final Member member = ctx.getMember();
-		final GuildVoiceState memberVoiceState = member.getVoiceState();
+		// Check if the user is in a voice channel
+		final GuildVoiceState memberVoiceState = ctx.getMember().getVoiceState();
 
 		if (!memberVoiceState.inVoiceChannel()) {
-			channel.sendMessage("You must be in a voice channel to use this command")
-				.delay(Duration.ofSeconds(10))
-				.flatMap(Message::delete)
-				.queue();
+			MessageMaker.timedMessage(
+				"You must be in a voice channel to use this command",
+				channel,
+				10
+			);
 			return;
 		}
 
 		// Check if the bot is in a voice channel
 		if (!selfVoiceState.inVoiceChannel()) {
-			channel.sendMessage("I must be in a voice channel to use this command")
-				.delay(Duration.ofSeconds(10))
-				.flatMap(Message::delete)
-				.queue();
-			return;
+			MessageMaker.timedMessage(
+				"I must be in a voice channel to use this command",
+				channel,
+				10
+			);
 		}
 
-		// Check if the bot has permission to join the voice channel
-		if (!self.hasPermission(Permission.VOICE_CONNECT)) {
-			channel.sendMessage("I don't have permission to join `" + memberVoiceState.getChannel().toString() + "`")
-				.delay(Duration.ofSeconds(10))
-				.flatMap(Message::delete)
-				.queue();
+		// Check if the bot is in another voice channel
+		if (selfVoiceState.getChannel() != memberVoiceState.getChannel()) {
+			MessageMaker.timedMessage(
+				"You must be in the same voice channel to use this command: `" +
+					selfVoiceState.getChannel().getName() + "`",
+				channel,
+				10
+			);
 			return;
 		}
 
@@ -62,10 +67,11 @@ public class Repeat implements CommandInterface {
 
 		// Check if track is playing
 		if (audioPlayer.getPlayingTrack() == null) {
-			channel.sendMessage("There's currently no track playing")
-				.delay(Duration.ofSeconds(10))
-				.flatMap(Message::delete)
-				.queue();
+			MessageMaker.timedMessage(
+				"There's currently no track playing",
+				channel,
+				10
+			);
 			return;
 		}
 
@@ -74,24 +80,25 @@ public class Repeat implements CommandInterface {
 
 		// Handle embed title whether music is repeating or not
 		String ifRepeat;
-		if (musicManager.scheduler.repeating) {
+		if (musicManager.scheduler.repeating)
 			ifRepeat = "\uD83D\uDD01 Now repeating track:";
-		} else {
+		else
 			ifRepeat = "\uD83D\uDEAB Stopped repeating track:";
-		}
 
 		// Get current track
 		AudioTrack track = musicManager.scheduler.player.getPlayingTrack();
 
 		// Send info embed
 		EmbedBuilder builder = new EmbedBuilder()
-			.setAuthor(ifRepeat, track.getInfo().uri, member.getUser().getAvatarUrl())
+			.setAuthor(ifRepeat, track.getInfo().uri, ctx.getMember().getUser().getAvatarUrl())
 			.setTitle(track.getInfo().title, track.getInfo().uri)
 			.setColor(RandomColor.getRandomColor());
 
 		// Send message
-		channel.sendMessageEmbeds(builder.build())
-			.queue();
+		channel.sendMessageEmbeds(builder.build()).queue();
+
+		// Delete original repeat command
+		ctx.getEvent().getMessage().delete().queue();
 	}
 
 	@Override
