0b3079e59602e5b91bb4c2ab8c95cce5e100f725
Raikuman <potatopiehotcakes@gmail.com>
2021-11-04 15:20:17 -0700

    Completed Stop Command Cleaned up command code and added documentation

--- src/main/java/com/reliquary/crow/commands/music/Stop.java
+++ src/main/java/com/reliquary/crow/commands/music/Stop.java
@@ -4,14 +4,17 @@ import com.reliquary.crow.commands.manager.CommandContext;
 import com.reliquary.crow.commands.manager.CommandInterface;
 import com.reliquary.crow.commands.music.manager.GuildMusicManager;
 import com.reliquary.crow.commands.music.manager.PlayerManager;
-import net.dv8tion.jda.api.Permission;
+import com.reliquary.crow.resources.MessageMaker;
 import net.dv8tion.jda.api.entities.GuildVoiceState;
 import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.TextChannel;
 
-import java.time.Duration;
-
+/**
+ * This class handles stopping the playing track and clearing the queue
+ *
+ * @version 1.0
+ * @since 2021-04-11
+ */
 @SuppressWarnings("ConstantConditions")
 public class Stop implements CommandInterface {
 
@@ -22,42 +25,42 @@ public class Stop implements CommandInterface {
 		final Member self = ctx.getSelfMember();
 		final GuildVoiceState selfVoiceState = self.getVoiceState();
 
-		// Check if a user is in a voice channel
-		final Member member = ctx.getMember();
-		final GuildVoiceState memberVoiceState = member.getVoiceState();
+		// Check if the user is in a voice channel
+		final GuildVoiceState memberVoiceState = ctx.getMember().getVoiceState();
 
 		if (!memberVoiceState.inVoiceChannel()) {
-			channel.sendMessage("You must be in a voice channel to use this command")
-				.delay(Duration.ofSeconds(10))
-				.flatMap(Message::delete)
-				.queue();
+			MessageMaker.timedMessage(
+				"You must be in a voice channel to use this command",
+				channel,
+				10
+			);
 			return;
 		}
 
 		// Check if the bot is in a voice channel
 		if (!selfVoiceState.inVoiceChannel()) {
-			channel.sendMessage("I must be in a voice channel to use this command")
-				.delay(Duration.ofSeconds(10))
-				.flatMap(Message::delete)
-				.queue();
+			MessageMaker.timedMessage(
+				"I must be in a voice channel to use this command",
+				channel,
+				10
+			);
+		}
+
+		// Check if the bot is in another voice channel
+		if (selfVoiceState.getChannel() != memberVoiceState.getChannel()) {
+			MessageMaker.timedMessage(
+				"You must be in the same voice channel to use this command: `" +
+					selfVoiceState.getChannel().getName() + "`",
+				channel,
+				10
+			);
 			return;
 		}
 
-		// Check if the bot has permission to join the voice channel
-		if (!self.hasPermission(Permission.VOICE_CONNECT)) {
-			channel.sendMessage("I don't have permission to join `" + memberVoiceState.getChannel().toString() + "`")
-				.delay(Duration.ofSeconds(10))
-				.flatMap(Message::delete)
-				.queue();
-			return;
-		}
-
-		// Get music manager
-		GuildMusicManager musicManager = PlayerManager.getInstance().getMusicManager(ctx.getGuild());
-
-		// Stop music
-		musicManager.scheduler.player.stopTrack();
+		// Clear playing track and queue
+		final GuildMusicManager musicManager = PlayerManager.getInstance().getMusicManager(ctx.getGuild());
 		musicManager.scheduler.queue.clear();
+		musicManager.scheduler.player.stopTrack();
 
 		// Send stop reaction
 		ctx.getEvent().getMessage()
