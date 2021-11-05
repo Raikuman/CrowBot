636187f4e9dc74f3d83f08522965661b75d5ba12
Raikuman <potatopiehotcakes@gmail.com>
2021-11-04 17:12:39 -0700

    Completed Pause Command Cleaned up command code and added documentation

--- src/main/java/com/reliquary/crow/commands/music/Pause.java
+++ src/main/java/com/reliquary/crow/commands/music/Pause.java
@@ -4,14 +4,18 @@ import com.reliquary.crow.commands.manager.CommandContext;
 import com.reliquary.crow.commands.manager.CommandInterface;
 import com.reliquary.crow.commands.music.manager.GuildMusicManager;
 import com.reliquary.crow.commands.music.manager.PlayerManager;
+import com.reliquary.crow.resources.MessageMaker;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import net.dv8tion.jda.api.entities.GuildVoiceState;
 import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.TextChannel;
 
-import java.time.Duration;
-
+/**
+ * This class handles pausing the playing track on the music manager
+ *
+ * @version 1.0
+ * @since 2021-04-11
+ */
 @SuppressWarnings("ConstantConditions")
 public class Pause implements CommandInterface {
 
@@ -22,24 +26,35 @@ public class Pause implements CommandInterface {
 		final Member self = ctx.getSelfMember();
 		final GuildVoiceState selfVoiceState = self.getVoiceState();
 
-		// Check if a user is in a voice channel
-		final Member member = ctx.getMember();
-		final GuildVoiceState memberVoiceState = member.getVoiceState();
+		// Check if the user is in a voice channel
+		final GuildVoiceState memberVoiceState = ctx.getMember().getVoiceState();
 
 		if (!memberVoiceState.inVoiceChannel()) {
-			channel.sendMessage("You must be in a voice channel to use this command")
-				.delay(Duration.ofSeconds(10))
-				.flatMap(Message::delete)
-				.queue();
+			MessageMaker.timedMessage(
+				"You must be in a voice channel to use this command",
+				channel,
+				10
+			);
 			return;
 		}
 
 		// Check if the bot is in a voice channel
 		if (!selfVoiceState.inVoiceChannel()) {
-			channel.sendMessage("I must be in a voice channel to use this command")
-				.delay(Duration.ofSeconds(10))
-				.flatMap(Message::delete)
-				.queue();
+			MessageMaker.timedMessage(
+				"I must be in a voice channel to use this command",
+				channel,
+				10
+			);
+		}
+
+		// Check if the bot is in another voice channel
+		if (selfVoiceState.getChannel() != memberVoiceState.getChannel()) {
+			MessageMaker.timedMessage(
+				"You must be in the same voice channel to use this command: `" +
+					selfVoiceState.getChannel().getName() + "`",
+				channel,
+				10
+			);
 			return;
 		}
 
@@ -49,10 +64,11 @@ public class Pause implements CommandInterface {
 
 		// Check if track is playing
 		if (audioPlayer.getPlayingTrack() == null) {
-			channel.sendMessage("There's currently no track playing")
-				.delay(Duration.ofSeconds(10))
-				.flatMap(Message::delete)
-				.queue();
+			MessageMaker.timedMessage(
+				"There's currently no track playing",
+				channel,
+				10
+			);
 			return;
 		}
 
