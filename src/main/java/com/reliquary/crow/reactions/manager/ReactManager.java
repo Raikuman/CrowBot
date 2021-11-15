51d0fdd0ed5e853524278cc5908732f30be2851e
Raikuman <potatopiehotcakes@gmail.com>
2021-11-14 23:00:25 -0800

    Added missing documentation to methods

--- src/main/java/com/reliquary/crow/reactions/manager/ReactManager.java
+++ src/main/java/com/reliquary/crow/reactions/manager/ReactManager.java
@@ -13,7 +13,7 @@ import java.util.List;
  * list while checking and handling the invocation for reactions
  *
  * @version 1.0
- * @since 2021-09-11
+ * @since 2021-14-11
  */
 public class ReactManager {
 
@@ -24,6 +24,10 @@ public class ReactManager {
 		addReact(new YamBoard());
 	}
 
+	/**
+	 * This method handles checking and adding a react to the list from the constructor
+	 * @param react Gets the react object for the list
+	 */
 	private void addReact(ReactInterface react) {
 
 		// Check if the react already exists in the list
@@ -38,10 +42,19 @@ public class ReactManager {
 		reactions.add(react);
 	}
 
+	/**
+	 * This method returns a list of all the reacts under the react manager
+	 * @return Returns a list of reacts
+	 */
 	public List<ReactInterface> getReactions() {
 		return reactions;
 	}
 
+	/**
+	 * This method gets a react using the search string to look for the react in the list
+	 * @param search The search string to find a react
+	 * @return Returns a react based on the search string, or null
+	 */
 	@Nullable
 	public ReactInterface getReact(String search) {
 
@@ -54,6 +67,11 @@ public class ReactManager {
 		return null;
 	}
 
+	/**
+	 * This method handles getting the add reaction from the event context and checking if the emote has a
+	 * react attached to it
+	 * @param event The react add event to build the context for the add react
+	 */
 	public void handleAdd(GuildMessageReactionAddEvent event) {
 
 		// Find the react using the event's reaction emote
@@ -64,6 +82,11 @@ public class ReactManager {
 			react.handleAdd(new ReactContext(event));
 	}
 
+	/**
+	 * This method handles getting the remove reaction from the event context and checking if the emote has a
+	 * react attached to it
+	 * @param event The react add event to build the context for the remove react
+	 */
 	public void handleRemove(GuildMessageReactionRemoveEvent event) {
 
 		// Find the react using the event's reaction emote
