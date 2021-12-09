17e3103a0149fef42cd77b6a69b21313020ad7ee
Raikuman <potatopiehotcakes@gmail.com>
2021-12-09 01:18:16 -0800

    Implemented Select Menu Manager Classes

---
+++ src/main/java/com/reliquary/crow/componentmanagers/selectionmenus/SelectInterface.java
@@ -0,0 +1,29 @@
+package com.reliquary.crow.componentmanagers.selectionmenus;
+
+/**
+ * This class provides an interface for creating select menu selections
+ *
+ * @version 1.0 2021-07-12
+ * @since 1.0
+ */
+public interface SelectInterface {
+
+	/**
+	 * This method is where all functions of the selection should go
+	 * @param ctx Uses the context of the selection event for command usage
+	 */
+	void handle (SelectContext ctx);
+
+	/**
+	 * This method returns the menu value (acts as an id) from the selection
+	 * @return Returns the menu value id string
+	 */
+	String getMenuValue();
+
+	/**
+	 * This method returns the label of the selection
+	 * @return Returns the selection label string
+	 */
+	String getLabel();
+
+}
