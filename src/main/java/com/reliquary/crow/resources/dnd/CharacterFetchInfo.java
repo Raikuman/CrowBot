5a011d1c0d6b01dd80cf26f44a72d35c520ac52d
Raikuman <40256283+Raikuman@users.noreply.github.com>
2022-01-10 14:53:25 -0800

    Changed fetch method to batch fetch method to reduce API call times

--- src/main/java/com/reliquary/crow/resources/dnd/CharacterFetchInfo.java
+++ src/main/java/com/reliquary/crow/resources/dnd/CharacterFetchInfo.java
@@ -1,40 +1,115 @@
 package com.reliquary.crow.resources.dnd;
 
-import com.reliquary.crow.resources.apis.googlesheets.SheetResources;
+import com.google.api.services.sheets.v4.model.BatchGetValuesResponse;
+import com.google.api.services.sheets.v4.model.ValueRange;
+import com.reliquary.crow.resources.apis.googlesheets.SheetProvider;
+
+import java.io.IOException;
+import java.util.*;
 
 /**
  * This class provides all methods to get character information from the provided sheetsId
  *
- * @version 1.1 2021-31-12
+ * @version 1.2 2022-10-01
  * @since 1.1
  */
 public class CharacterFetchInfo {
 
 	private final String sheetsId;
+	public LinkedHashMap<String, String> characterMap;
 
 	public CharacterFetchInfo(String sheetsId) {
 		this.sheetsId = sheetsId;
+
+		characterMap = new LinkedHashMap<>(Map.of(
+			"name", "v2.1!C6",
+			"race", "v2.1!T7",
+			"backstory", "v2.1!R165:R177",
+			"portrait", "v2.1!C176",
+			"alignment", "v2.1!AJ28",
+			"level", "v2.1!AL6",
+			"class", "v2.1!T5"
+		));
 	}
 
-	public String characterName() {
+	/**
+	 * This method batch fetches info from the Sheets and returns the response in a hashmap
+	 * @param neededList Provides the list of items to fetch
+	 * @return Returns a hashmap of the response
+	 */
+	public HashMap<String, String> batchFetchInfo(List<String> neededList) {
 
-		String characterName = SheetResources.fetchInfo(sheetsId, "v2.1!C6");
+		List<String> ranges = new ArrayList<>();
+		HashMap<String, String> valueMap = new LinkedHashMap<>();
 
-		if (characterName.isEmpty())
-			return "Unnamed Character";
-		else
-			return characterName;
+		for (String needed : neededList) {
+			if (characterMap.containsKey(needed)) {
+				valueMap.put(needed, characterMap.get(needed));
+				ranges.add(characterMap.get(needed));
+			}
+		}
+
+		try {
+			BatchGetValuesResponse response = SheetProvider.getInstance().sheetService.spreadsheets().values()
+				.batchGet(sheetsId).setRanges(ranges).execute();
+
+			int index = 0;
+			StringBuilder stringBuilder;
+			for (ValueRange range : response.getValueRanges()) {
+
+				stringBuilder = new StringBuilder();
+
+				if (range.getValues() == null) {
+					stringBuilder.append("null");
+				} else {
+					for (List<Object> objectList : range.getValues()) {
+
+						stringBuilder.append(objectList.get(0).toString());
+
+						if (objectList.get(0).toString().endsWith("."))
+							stringBuilder.append("\n");
+						else
+							stringBuilder.append(" ");
+					}
+				}
+
+				valueMap.put(neededList.get(index), stringBuilder.toString());
+				index++;
+			}
+		} catch (IOException e) {
+			return null;
+		}
+
+		return updateMapDefaults(valueMap);
 	}
 
-	public String characterPortrait() {
+	/**
+	 * This method sets the response data to default values if there was no data found
+	 * @param responseMap Provides the hashmap to change defaults in
+	 * @return Returns a hashmap of the updated response
+	 */
+	private HashMap<String, String> updateMapDefaults(HashMap<String, String> responseMap) {
 
-		String characterPortrait = SheetResources.fetchInfo(sheetsId, "v2.1!C176");
+		HashMap<String, String> defaultMap = new HashMap<>(Map.of(
+			"name", "Unnamed Character",
+			"race", "Unknown",
+			"backstory", "",
+			"portrait", "",
+			"alignment", "Unknown",
+			"level", "Unknown",
+			"class", "Unknown"
+		));
 
-		if (characterPortrait.isEmpty())
-			return "";
+		HashMap<String, String> outputMap = new HashMap<>();
 
-		String fileId = characterPortrait.split("=")[characterPortrait.split("=").length-1];
+		for (Map.Entry<String, String> entry : responseMap.entrySet()) {
 
-		return "https://drive.google.com/uc?id=" + fileId;
+			if (entry.getValue().equals("null"))
+				outputMap.put(entry.getKey(), defaultMap.get(entry.getKey()));
+			else
+				outputMap.put(entry.getKey(), entry.getValue());
+		}
+
+		return outputMap;
 	}
 }
