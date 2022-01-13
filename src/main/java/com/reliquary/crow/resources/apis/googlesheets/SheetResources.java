dfbbf0f4909670ac4d994c0da0126b50b98ca6ab
Raikuman <40256283+Raikuman@users.noreply.github.com>
2022-01-13 00:43:43 -0800

    Moved fetch methods to SheetResources class to generalize fetching to any sheet

--- src/main/java/com/reliquary/crow/resources/apis/googlesheets/SheetResources.java
+++ src/main/java/com/reliquary/crow/resources/apis/googlesheets/SheetResources.java
@@ -1,17 +1,20 @@
 package com.reliquary.crow.resources.apis.googlesheets;
 
 import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.model.BatchGetValuesResponse;
 import com.google.api.services.sheets.v4.model.ValueRange;
 import com.reliquary.crow.resources.dnd.CharacterFetchInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * This class handles in providing methods to help to use and checking the SheetsProvider.java class
  *
- * @version 1.1 2021-31-12
+ * @version 1.2 2022-12-01
  * @since 1.1
  */
 public class SheetResources {
@@ -54,26 +57,94 @@ public class SheetResources {
 	}
 
 	/**
-	 * This method fetches information from the provided sheets link and range
-	 * @param sheetsId Provides the full sheets link url
-	 * @param range Provides the range to get values from the sheet
-	 * @return Returns a string of the info fetched from the sheet
+	 * This method returns a hashmap of a cell range from Google Sheets
+	 * @param sheetsId Provides the Google Sheets id
+	 * @param rangeMap Provides the rangeMap to search for cells
+	 * @param paramList Provides the parameters to get from Google Sheets
+	 * @return Returns a hashmap of the response
 	 */
-	public static String fetchInfo(String sheetsId, String range) {
-		try {
-			ValueRange response = SheetProvider.getInstance().sheetService.spreadsheets().values()
-				.get(sheetsId, range)
-				.execute();
+	public static HashMap<String, List<List<String>>> fetchInfoCellRange(String sheetsId,
+		HashMap<String, String> rangeMap, List<String> paramList) {
 
-			if (response.getValues() == null)
-				return "";
-			else
-				return response.getValues().get(0).get(0).toString();
+		List<String> ranges = new ArrayList<>();
+		HashMap<String, List<List<String>>> characterMap = new HashMap<>();
+
+		for (String param : paramList)
+			if (rangeMap.containsKey(param))
+				ranges.add(rangeMap.get(param));
+
+		try {
+			BatchGetValuesResponse response = SheetProvider.getInstance().sheetService.spreadsheets().values()
+				.batchGet(sheetsId).setRanges(ranges).execute();
+
+			int index = 0;
+			for (ValueRange values : response.getValueRanges()) {
+				// Set default cases
+				if (values.getValues() == null) {
+					characterMap.put(paramList.get(index), List.of(List.of("null")));
+				} else {
+					characterMap.put(paramList.get(index), values.getValues().stream().map(
+						listObj -> listObj.stream().map(
+							obj -> Objects.toString(obj, null)
+						).collect(Collectors.toList())
+					).collect(Collectors.toList()));
+				}
+
+				index++;
+			}
 		} catch (IOException e) {
 			logger.info(
-				"Could not fetch data from id: [" + sheetsId + "] with range [" + range + "]"
+				"Could not access information from sheet: [" + sheetsId + "]"
 			);
-			return "";
 		}
+
+		return characterMap;
+	}
+
+	/**
+	 * This method compresses the fetchInfoCellRange method so that the values are a single string
+	 * @param sheetsId Provides the Google Sheets id
+	 * @param rangeMap Provides the rangeMap to search for cells
+	 * @param defaults Provides a default map to set default for empty values
+	 * @param paramList Provides the parameters to get from Google Sheets
+	 * @return Returns a hashmap of the compressed response
+	 */
+	public static HashMap<String, String> fetchInfoCell(String sheetsId, HashMap<String, String> rangeMap,
+		HashMap<String, String> defaults, List<String> paramList) {
+
+		HashMap<String, String> characterMap = new HashMap<>();
+		HashMap<String, List<List<String>>> cellRange = fetchInfoCellRange(
+			sheetsId,
+			rangeMap,
+			paramList
+		);
+
+		for (Map.Entry<String, List<List<String>>> entry : cellRange.entrySet())
+			if (entry.getValue().size() == 1)
+				if (entry.getValue().get(0).size() == 1)
+					characterMap.put(entry.getKey(), entry.getValue().get(0).get(0));
+
+		return updateMapDefaults(characterMap, defaults);
+	}
+
+	/**
+	 *  This method sets the response data to default values if there was no data found
+	 * @param characterMap Provides the hashmap to change defaults in
+	 * @param defaults Provides a default map to set default for empty values
+	 * @return Returns a hashmap of the updated response
+	 */
+	private static HashMap<String, String> updateMapDefaults(HashMap<String, String> characterMap,
+		HashMap<String, String> defaults) {
+
+		HashMap<String, String> updatedCharacterMap = new HashMap<>();
+
+		for (Map.Entry<String, String> entry : characterMap.entrySet()) {
+			if (entry.getValue().equals("null"))
+				updatedCharacterMap.put(entry.getKey(), defaults.get(entry.getKey()));
+			else
+				updatedCharacterMap.put(entry.getKey(), entry.getValue());
+		}
+
+		return updatedCharacterMap;
 	}
 }
--- src/main/java/com/reliquary/crow/resources/dnd/CharacterFetchInfo.java
+++ src/main/java/com/reliquary/crow/resources/dnd/CharacterFetchInfo.java
@@ -1,110 +1,20 @@
 package com.reliquary.crow.resources.dnd;
 
-import com.google.api.services.sheets.v4.model.BatchGetValuesResponse;
-import com.google.api.services.sheets.v4.model.ValueRange;
-import com.reliquary.crow.resources.apis.googlesheets.SheetProvider;
-
-import java.io.IOException;
 import java.util.*;
-import java.util.stream.Collectors;
 
 /**
  * This class provides all methods to get character information from the provided sheetsId
  *
- * @version 2.0 2022-12-01
+ * @version 3.0 2022-12-01
  * @since 1.1
  */
 public class CharacterFetchInfo {
-
-	private final String sheetsId;
-
-	public CharacterFetchInfo(String sheetsId) {
-		this.sheetsId = sheetsId;
-	}
-
-	/**
-	 * This method returns a hashmap of a cell range from Google Sheets
-	 * @param paramList Provides the parameters to get from Google Sheets
-	 * @return Returns a hashmap of the response
-	 */
-	public HashMap<String, List<List<String>>> fetchInfoCellRange(List<String> paramList) {
-
-		List<String> ranges = new ArrayList<>();
-		HashMap<String, List<List<String>>> characterMap = new HashMap<>();
-
-		for (String param : paramList)
-			if (getRangeMap().containsKey(param))
-				ranges.add(getRangeMap().get(param));
-
-		try {
-			BatchGetValuesResponse response = SheetProvider.getInstance().sheetService.spreadsheets().values()
-				.batchGet(sheetsId).setRanges(ranges).execute();
-
-			int index = 0;
-			for (ValueRange values : response.getValueRanges()) {
-
-				// Default case
-				if (values.getValues() == null) {
-					characterMap.put(paramList.get(index), List.of(List.of("null")));
-				} else {
-					characterMap.put(paramList.get(index), values.getValues().stream().map(
-						listObj -> listObj.stream().map(
-							obj -> Objects.toString(obj, null)
-						).collect(Collectors.toList())
-					).collect(Collectors.toList()));
-				}
-
-				index++;
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		return characterMap;
-	}
-
-	/**
-	 * This method compresses the fetchInfoCellRange method so that the values are a single string
-	 * @param paramList Provides the parameters to get from Google Sheets
-	 * @return Returns a hashmap of the compressed response
-	 */
-	public HashMap<String, String> fetchInfoCell(List<String> paramList) {
-
-		HashMap<String, String> characterMap = new HashMap<>();
-		HashMap<String, List<List<String>>> cellRange = fetchInfoCellRange(paramList);
-
-		for (Map.Entry<String, List<List<String>>> entry : cellRange.entrySet())
-			if (entry.getValue().size() == 1)
-				if (entry.getValue().get(0).size() == 1)
-					characterMap.put(entry.getKey(), entry.getValue().get(0).get(0));
-
-		return updateMapDefaults(characterMap);
-	}
-
-	/**
-	 * This method sets the response data to default values if there was no data found
-	 * @param characterMap Provides the hashmap to change defaults in
-	 * @return Returns a hashmap of the updated response
-	 */
-	private HashMap<String, String> updateMapDefaults(HashMap<String, String> characterMap) {
-
-		HashMap<String, String> updatedCharacterMap = new HashMap<>();
-
-		for (Map.Entry<String, String> entry : characterMap.entrySet()) {
-			if (entry.getValue().equals("null"))
-				updatedCharacterMap.put(entry.getKey(), defaultMap().get(entry.getKey()));
-			else
-				updatedCharacterMap.put(entry.getKey(), entry.getValue());
-		}
-
-		return updatedCharacterMap;
-	}
 
 	/**
 	 * This method returns the map of ranges to get from Google Sheets
 	 * @return Returns a hashmap of ranges
 	 */
-	private HashMap<String, String> getRangeMap() {
+	public static HashMap<String, String> getRangeMap() {
 
 		return new HashMap<>(Map.ofEntries(
 			Map.entry("name", "v2.1!C6"),
@@ -114,6 +24,30 @@ public class CharacterFetchInfo {
 			Map.entry("alignment", "v2.1!AJ28"),
 			Map.entry("level", "v2.1!AL6"),
 			Map.entry("class", "v2.1!T5"),
+
+			Map.entry("proficiencybonus", "v2.1!H14:I15"),
+			Map.entry("weaponproficiencies", "v2.1!I50:N50"),
+			Map.entry("armorclass", "v2.1!R12:S13"),
+			Map.entry("initiative", "v2.1!V12:W13"),
+			Map.entry("speed", "v2.1!Z12:AA13"),
+			Map.entry("healthmax", "v2.1!U16:AA16"),
+			Map.entry("healthcurrent", "v2.1!R17:AA18"),
+
+			Map.entry("background", "v2.1!AJ11:AN11"),
+			Map.entry("personalitytraits", "v2.1!AE12:AN14"),
+			Map.entry("ideals", "v2.1!AE16:AN18"),
+			Map.entry("bonds", "v2.1!AE20:AN22"),
+			Map.entry("flaws", "v2.1!AE24:AN26"),
+
+			Map.entry("age", "v2.1!C148:E148"),
+			Map.entry("height", "v2.1!F148:H148"),
+			Map.entry("weight", "v2.1!I148:K148"),
+			Map.entry("size", "v2.1!L148:N148"),
+			Map.entry("gender", "v2.1!C150:E150"),
+			Map.entry("eyes", "v2.1!F150:H150"),
+			Map.entry("hair", "v2.1!I150:K150"),
+			Map.entry("skin", "v2.1!L150:N150"),
+
 			Map.entry("attacks1", "v2.1!AQ32:BC35"),
 			Map.entry("attacks2", "Additional!AQ3:BC24")
 		));
@@ -123,16 +57,33 @@ public class CharacterFetchInfo {
 	 * This method returns the map of defaults for keys
 	 * @return Returns a hashmap of defaults
 	 */
-	private HashMap<String, String> defaultMap() {
+	public static HashMap<String, String> defaultMap() {
 
 		return new HashMap<>(Map.ofEntries(
 			Map.entry("name", "Unnamed Character"),
 			Map.entry("race", "Unknown"),
-			Map.entry("backstory", ""),
 			Map.entry("portrait", ""),
 			Map.entry("alignment", "Unknown"),
 			Map.entry("level", "Unknown"),
-			Map.entry("class", "Unknown")
+			Map.entry("class", "Unknown"),
+
+			Map.entry("proficiencybonus", "0"),
+			Map.entry("weaponproficiencies", ""),
+			Map.entry("armorclass", "0"),
+			Map.entry("initiative", "0"),
+			Map.entry("speed", "0 ft"),
+			Map.entry("healthmax", "0"),
+			Map.entry("healthcurrent", "0"),
+
+			Map.entry("background", "Unknown"),
+			Map.entry("age", "Unknown"),
+			Map.entry("height", "Unknown"),
+			Map.entry("weight", "Unknown"),
+			Map.entry("size", "Unknown"),
+			Map.entry("gender", "Unknown"),
+			Map.entry("eyes", "Unknown"),
+			Map.entry("hair", "Unknown0"),
+			Map.entry("skin", "Unknown")
 		));
 	}
 }
