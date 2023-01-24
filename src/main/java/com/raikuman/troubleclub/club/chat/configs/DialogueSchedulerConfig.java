fdbd61eb27af6680f9f477c46e6611466403acc9
Raikuman <40256283+Raikuman@users.noreply.github.com>
2023-01-23 20:29:49 -0800

    Change locations of configs for chat functionality

--- src/main/java/com/raikuman/troubleclub/club/chat/configs/DialogueSchedulerConfig.java
+++ src/main/java/com/raikuman/troubleclub/club/chat/configs/DialogueSchedulerConfig.java
@@ -4,6 +4,12 @@ import com.raikuman.botutilities.configs.ConfigInterface;
 
 import java.util.LinkedHashMap;
 
+/**
+ * Provides configuration for dialogue scheduling
+ *
+ * @version 1.1 2023-23-01
+ * @since 1.0
+ */
 public class DialogueSchedulerConfig implements ConfigInterface {
 
 	@Override
@@ -14,6 +20,13 @@ public class DialogueSchedulerConfig implements ConfigInterface {
 	@Override
 	public LinkedHashMap<String, String> getConfigs() {
 		LinkedHashMap<String, String> configMap = new LinkedHashMap<>();
+		configMap.put("dialoguechannelid", "");
+		configMap.put("minScheduledDays", "1");
+		configMap.put("bypassRandomAddedDays", "false");
+		configMap.put("crowsticker", "");
+		configMap.put("dessticker", "");
+		configMap.put("suusticker", "");
+		configMap.put("inoristicker", "");
 		configMap.put("selectdate", "1999-01-01");
 
 		return configMap;
--- src/main/java/com/raikuman/troubleclub/club/chat/configs/ReplyConfig.java
+++ src/main/java/com/raikuman/troubleclub/club/chat/configs/ReplyConfig.java
@@ -7,25 +7,20 @@ import java.util.LinkedHashMap;
 /**
  * Provides configuration for chat
  *
- * @version 1.1 2023-20-01
+ * @version 1.2 2023-23-01
  * @since 1.0
  */
-public class ChatConfig implements ConfigInterface {
+public class ReplyConfig implements ConfigInterface {
 
 	@Override
 	public String fileName() {
-		return "chat";
+		return "reply";
 	}
 
 	@Override
 	public LinkedHashMap<String, String> getConfigs() {
 		LinkedHashMap<String, String> configMap = new LinkedHashMap<>();
-		configMap.put("replycommandchannelid", "");
-		configMap.put("dialoguechannelid", "");
-		configMap.put("crowsticker", "");
-		configMap.put("dessticker", "");
-		configMap.put("suusticker", "");
-		configMap.put("inoristicker", "");
+		configMap.put("commandchannelid", "");
 
 		return configMap;
 	}
