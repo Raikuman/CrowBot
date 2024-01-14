9968cc18ef36ee45e67b667682d8c80952e3a85e
Raikuman <40256283+Raikuman@users.noreply.github.com>
2024-01-14 15:22:46 -0800

    Update project

--- src/main/java/com/raikuman/troubleclub/TroubleClub.java
+++ src/main/java/com/raikuman/troubleclub/TroubleClub.java
@@ -1,5 +1,12 @@
 package com.raikuman.troubleclub;
 
+import com.raikuman.botutilities.BotSetup;
+import com.raikuman.botutilities.invocation.type.Command;
+import com.raikuman.troubleclub.command.GetStickers;
+import com.raikuman.troubleclub.command.TestDialogue;
+import com.raikuman.troubleclub.dialogue.*;
+import com.raikuman.troubleclub.dialogue.config.DialogueConfig;
+import com.raikuman.troubleclub.dialogue.config.DialogueWeightsConfig;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
@@ -9,39 +16,84 @@ import net.dv8tion.jda.api.utils.cache.CacheFlag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 public class TroubleClub {
 
     private static final Logger logger = LoggerFactory.getLogger(TroubleClub.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        // Schedule tasks
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(2);
+        DialogueManager dialogueManager = new DialogueManager(executor);
+        dialogueManager.beginTask();
+
         HashMap<Club, JDA> clubMap = new HashMap<>();
         for (Club club : Club.values()) {
+            //if (club != Club.SUU) continue;
+
             try {
-                clubMap.put(
-                    club,
-                    JDABuilder
-                        .createDefault(System.getenv(club + "TOKEN"))
-                        .enableIntents(List.of(
-                            GatewayIntent.GUILD_MEMBERS,
-                            GatewayIntent.GUILD_MESSAGES,
-                            GatewayIntent.MESSAGE_CONTENT))
-                        .setChunkingFilter(ChunkingFilter.ALL)
-                        .enableCache(CacheFlag.VOICE_STATE)
-                        //.addEventListeners()
-                        .setMemberCachePolicy(MemberCachePolicy.ALL)
-                        .setMaxReconnectDelay(32)
-                        .setAutoReconnect(true)
-                        .setRequestTimeoutRetry(true)
-                        .build()
-                );
+                JDABuilder jdaBuilder = JDABuilder
+                    .create(List.of(
+                        GatewayIntent.GUILD_MEMBERS,
+                        GatewayIntent.GUILD_MESSAGES,
+                        GatewayIntent.GUILD_EMOJIS_AND_STICKERS,
+                        GatewayIntent.GUILD_VOICE_STATES,
+                        GatewayIntent.MESSAGE_CONTENT))
+                    .setChunkingFilter(ChunkingFilter.ALL)
+                    .enableCache(CacheFlag.VOICE_STATE)
+                    //.addEventListeners()
+                    .setMemberCachePolicy(MemberCachePolicy.ALL)
+                    .setMaxReconnectDelay(32)
+                    .setAutoReconnect(true)
+                    .setRequestTimeoutRetry(true);
+
+                BotSetup setup = BotSetup
+                    .setup(jdaBuilder)
+                    .disableDatabase(true)
+                    .setConfigs(new DialogueConfig(), new DialogueWeightsConfig());
+
+                // Handle commands
+                List<Command> commands = getCommands(dialogueManager).get(club);
+                if (commands != null) {
+                    setup = setup.addCommands(commands);
+                }
+
+
+                clubMap.put(club, setup.build(System.getenv(club + "TOKEN")));
             } catch (IllegalArgumentException e) {
                 logger.error("Failed to initialize " + club + " bot: " + e.getMessage());
             }
         }
 
+        File resourceDirectory = new File("resources");
+        if (resourceDirectory.mkdirs()) {
+            logger.info("Created resources directory");
+        } else {
+            logger.error("Failed to create resources directory");
+        }
 
+        // Program must load with all bots
+        if (clubMap.size() != Club.values().length) {
+            logger.error("Failed to initialize all bots");
+            System.exit(0);
+        }
+
+        dialogueManager.setClubMap(clubMap);
+    }
+
+    private static HashMap<Club, List<Command>> getCommands(DialogueManager dialogueManager) {
+        HashMap<Club, List<Command>> commands = new HashMap<>();
+        commands.put(Club.SUU, List.of(
+            new GetStickers(),
+            new TestDialogue(dialogueManager)
+        ));
+
+        return commands;
     }
 }
