e872b20b199ea8731183c0d538fd50c85df112cd
Raikuman <40256283+Raikuman@users.noreply.github.com>
2024-04-04 00:01:17 -0700

    Implement builder for tamagopet buttons

--- src/main/java/com/raikuman/troubleclub/tamagopet/event/TamagopetButtonBuilder.java
+++ src/main/java/com/raikuman/troubleclub/tamagopet/event/TamagopetButtonBuilder.java
@@ -1,18 +1,20 @@
 package com.raikuman.troubleclub.tamagopet.event;
 
-import com.raikuman.botutilities.invocation.type.SelectComponent;
+import com.raikuman.botutilities.invocation.type.ButtonComponent;
 import com.raikuman.troubleclub.tamagopet.TamagopetDatabaseHandler;
 import com.raikuman.troubleclub.tamagopet.TamagopetEnum;
 import com.raikuman.troubleclub.tamagopet.TamagopetManager;
 import com.raikuman.troubleclub.tamagopet.image.TamagopetImage;
-import net.dv8tion.jda.api.events.interaction.component.StringSelectInteractionEvent;
+import net.dv8tion.jda.api.entities.emoji.Emoji;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+import net.dv8tion.jda.api.interactions.components.buttons.ButtonStyle;
 import org.slf4j.helpers.CheckReturnValue;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class TamagopetSelectBuilder {
+public class TamagopetButtonBuilder {
 
     private final TamagopetManager tamagopetManager;
     private final TamagopetEnum tamagopetEnum;
@@ -20,11 +22,13 @@ public class TamagopetSelectBuilder {
     private String baseImage, imposeImage;
     private int happiness, health, x, y;
     private List<String> titles, descriptions;
+    private Emoji emoji;
+    private ButtonStyle buttonStyle;
 
-    private TamagopetSelectBuilder(String label, TamagopetManager tamagopetManager, TamagopetEnum tamagopetEnum) {
-        this.tamagopetEnum = tamagopetEnum;
-        this.tamagopetManager = tamagopetManager;
+    public TamagopetButtonBuilder(String label, TamagopetManager tamagopetManager, TamagopetEnum tamagopetEnum) {
         this.label = label;
+        this.tamagopetManager = tamagopetManager;
+        this.tamagopetEnum = tamagopetEnum;
         this.baseImage = "";
         this.imposeImage = "";
         this.happiness = 0;
@@ -33,21 +37,23 @@ public class TamagopetSelectBuilder {
         this.y = 0;
         this.titles = new ArrayList<>();
         this.descriptions = new ArrayList<>();
+        this.emoji = null;
+        this.buttonStyle = null;
     }
 
     @CheckReturnValue
-    public static TamagopetSelectBuilder setup(String label, TamagopetManager tamagopetManager, TamagopetEnum tamagopetEnum) {
-        return new TamagopetSelectBuilder(label, tamagopetManager, tamagopetEnum);
+    public static TamagopetButtonBuilder setup(String label, TamagopetManager tamagopetManager, TamagopetEnum tamagopetEnum) {
+        return new TamagopetButtonBuilder(label, tamagopetManager, tamagopetEnum);
     }
 
     @CheckReturnValue
-    public TamagopetSelectBuilder setBaseImage(String baseImage) {
+    public TamagopetButtonBuilder setBaseImage(String baseImage) {
         this.baseImage = baseImage;
         return this;
     }
 
     @CheckReturnValue
-    public TamagopetSelectBuilder setImposeImage(String imposeImage, int x, int y) {
+    public TamagopetButtonBuilder setImposeImage(String imposeImage, int x, int y) {
         this.imposeImage = imposeImage;
         this.x = x;
         this.y = y;
@@ -55,47 +61,59 @@ public class TamagopetSelectBuilder {
     }
 
     @CheckReturnValue
-    public TamagopetSelectBuilder setImposeImage(String imposeImage) {
+    public TamagopetButtonBuilder setImposeImage(String imposeImage) {
         this.imposeImage = imposeImage;
         return this;
     }
 
     @CheckReturnValue
-    public TamagopetSelectBuilder setTitles(List<String> titles) {
+    public TamagopetButtonBuilder setTitles(List<String> titles) {
         this.titles = titles;
         return this;
     }
 
     @CheckReturnValue
-    public TamagopetSelectBuilder setDescriptions(List<String> descriptions) {
+    public TamagopetButtonBuilder setDescriptions(List<String> descriptions) {
         this.descriptions = descriptions;
         return this;
     }
 
     @CheckReturnValue
-    public TamagopetSelectBuilder setHappiness(int happiness) {
+    public TamagopetButtonBuilder setEmoji(Emoji emoji) {
+        this.emoji = emoji;
+        return this;
+    }
+
+    @CheckReturnValue
+    public TamagopetButtonBuilder setButtonStyle(ButtonStyle buttonStyle) {
+        this.buttonStyle = buttonStyle;
+        return this;
+    }
+
+    @CheckReturnValue
+    public TamagopetButtonBuilder setHappiness(int happiness) {
         this.happiness = happiness;
         return this;
     }
 
     @CheckReturnValue
-    public TamagopetSelectBuilder setHealth(int health) {
+    public TamagopetButtonBuilder setHealth(int health) {
         this.health = health;
         return this;
     }
 
-    public SelectComponent build() {
+    public ButtonComponent build() {
         TamagopetImage image = new TamagopetImage(baseImage);
         if (!imposeImage.isEmpty()) {
             image.superimpose(imposeImage, x, y);
         }
 
-        return new SelectComponent() {
+        return new ButtonComponent() {
 
             private final Random random = new Random();
 
             @Override
-            public void handle(StringSelectInteractionEvent ctx) {
+            public void handle(ButtonInteractionEvent ctx) {
                 // Handle data
                 tamagopetManager.addHappiness(ctx.getChannel(), happiness);
                 tamagopetManager.reduceHealth(ctx.getChannel(), health);
@@ -134,7 +152,7 @@ public class TamagopetSelectBuilder {
 
             @Override
             public String getInvoke() {
-                return "tamagopetselect" + label;
+                return "tamagopetbutton" + label;
             }
 
             @Override
@@ -143,8 +161,21 @@ public class TamagopetSelectBuilder {
             }
 
             @Override
-            public boolean ignoreAuthor() {
-                return true;
+            public Emoji displayEmoji() {
+                if (emoji != null) {
+                    return emoji;
+                } else {
+                    return super.displayEmoji();
+                }
+            }
+
+            @Override
+            public ButtonStyle buttonStyle() {
+                if (buttonStyle != null) {
+                    return buttonStyle;
+                } else {
+                    return super.buttonStyle();
+                }
             }
         };
     }
